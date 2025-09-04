b71708fe663859fc72d65c4c6950e0d04cbf2cc3
kauan araujo <kauanraujo340@gmail.com>
2025-09-04 14:02:13 -0300

    Corrigindo atividade

--- variaveisModulo8/src/TarafaCalculoMedia/calculoMedia.java
+++ variaveisModulo8/src/TarafaCalculoMedia/calculoMedia.java
@@ -5,26 +5,21 @@ public class calculoMedia {
     public static void main(String args[]){
 
         calculadorMedia();
-
     }
 
     private static void calculadorMedia() {
         System.out.println(" **** calculadorMedia ***");
 
-        double nota1 = 8;
-        double nota2 = 6;
-        double nota3 = 10;
-        double nota4 = 5;
+        int nota1 = 7;
+        int nota2 = 5;
+        int nota3 = 10;
+        int nota4 = 9;
 
-        double mediaNota1 = nota1 / 4;
-        double mediaNota2 = nota2 / 4;
-        double mediaNota3 = nota3 / 4;
-        double mediaNota4 = nota4 / 4;
+        int totalNotas = nota1 + nota2 + nota3 + nota4;
 
-        System.out.println(mediaNota1);
-        System.out.println(mediaNota2);
-        System.out.println(mediaNota3);
-        System.out.println(mediaNota4);
+        double mediaNota = (double) totalNotas /4;
 
+        System.out.println(mediaNota);
     }
+
 }
