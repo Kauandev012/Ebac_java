a37a9e18bdc8eb6b81ee0deb460149c0bdcda165
kauan araujo <kauanraujo340@gmail.com>
2025-09-04 13:20:56 -0300

    calculadoraMedia

---
+++ variaveisModulo8/src/TarafaCalculoMedia/calculoMedia.java
@@ -0,0 +1,30 @@
+package TarafaCalculoMedia;
+
+public class calculoMedia {
+
+    public static void main(String args[]){
+
+        calculadorMedia();
+
+    }
+
+    private static void calculadorMedia() {
+        System.out.println(" **** calculadorMedia ***");
+
+        double nota1 = 8;
+        double nota2 = 6;
+        double nota3 = 10;
+        double nota4 = 5;
+
+        double mediaNota1 = nota1 / 4;
+        double mediaNota2 = nota2 / 4;
+        double mediaNota3 = nota3 / 4;
+        double mediaNota4 = nota4 / 4;
+
+        System.out.println(mediaNota1);
+        System.out.println(mediaNota2);
+        System.out.println(mediaNota3);
+        System.out.println(mediaNota4);
+
+    }
+}
